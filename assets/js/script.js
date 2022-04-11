2b3ad6e90efacaab290fc6db6e3cc0599997f83f
Adeline Izri <adeline.rudnicki@gmail.com>
2022-04-11 13:44:36 +0200

    cv suite 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -131,7 +131,7 @@ fetch('./assets/data/cv.json')
                 <div class="row">
                     <div class="col-12 d-flex justify-content-center ps-5">
                         <div class="iconsImg">
-                            <img class="img-fluid" src="${element.image}">
+                            <img class="img-fluid" src="${element.image}" alt="${element.alt}">
                         </div>
                     </div>
                     <div class="col-12 d-flex justify-content-center ps-5 nameElement">
@@ -145,11 +145,11 @@ fetch('./assets/data/cv.json')
         if (element.part == 'Back-End') {
             // let newStarRating = starGrade(element.grade);
             backEndSkillsBody.innerHTML += `
-            <div class="col-12 col-lg-4 ps-5" id="${element.id}">
+            <div class="col-12 col-lg-6 ps-5" id="${element.id}">
                 <div class="row">
                     <div class="col-12 d-flex justify-content-center ps-5">
                         <div class="iconsImg">
-                            <img class="img-fluid" src="${element.image}">
+                            <img class="img-fluid" src="${element.image}" alt="${element.alt}">
                         </div>
                     </div>
                     <div class="col-12 d-flex justify-content-center ps-5 nameElement">
@@ -163,11 +163,11 @@ fetch('./assets/data/cv.json')
         if (element.part == 'CMS') {
             // let newStarRating = starGrade(element.grade);
             cmsSkillsBody.innerHTML += `
-            <div class="col-12 col-lg-4 ps-5" id="${element.id}">
+            <div class="col-12 ps-5" id="${element.id}">
                 <div class="row">
                     <div class="col-12 d-flex justify-content-center ps-5">
                         <div class="iconsImg">
-                            <img class="img-fluid" src="${element.image}">
+                            <img class="img-fluid" src="${element.image}" alt="${element.alt}">
                         </div>
                     </div>
                     <div class="col-12 d-flex justify-content-center ps-5 nameElement">
@@ -181,11 +181,11 @@ fetch('./assets/data/cv.json')
         if (element.part == 'Web design') {
             // let newStarRating = starGrade(element.grade);
             webDesignSkillsBody.innerHTML += `
-            <div class="col-12 col-lg-4 ps-5" id="${element.id}">
+            <div class="col-12 ps-5" id="${element.id}">
                 <div class="row">
                     <div class="col-12 d-flex justify-content-center ps-5">
                         <div class="iconsImg">
-                            <img class="img-fluid" src="${element.image}">
+                            <img class="img-fluid" src="${element.image}" alt="${element.alt}">
                         </div>
                     </div>
                     <div class="col-12 d-flex justify-content-center ps-5 nameElement">
@@ -199,11 +199,11 @@ fetch('./assets/data/cv.json')
         if (element.part == 'Environnement de travail') {
             // let newStarRating = starGrade(element.grade);
             environmentSkillsBody.innerHTML += `
-            <div class="col-12 col-lg-4 ps-5" id="${element.id}">
+            <div class="col-12 col-lg-6 ps-5" id="${element.id}">
                 <div class="row">
                     <div class="col-12 d-flex justify-content-center ps-5">
                         <div class="iconsImg">
-                            <img class="img-fluid" src="${element.image}">
+                            <img class="img-fluid" src="${element.image}" alt="${element.alt}">
                         </div>
                     </div>
                     <div class="col-12 d-flex justify-content-center ps-5 nameElement">
